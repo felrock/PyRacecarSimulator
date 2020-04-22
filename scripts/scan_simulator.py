e40f9d79cda5a05fa53dac9113d252e34b80e5aa
Felix <stenbackfelix@gmail.com>
2020-04-22 11:25:38 +0200

    moved ros and pass map instead

--- scripts/scan_simulator.py
+++ scripts/scan_simulator.py
@@ -9,65 +9,83 @@ import random
 
 class ScanSimulator2D:
 
-    def __init__(self, num_rays, fov, scan_std, rt_e, theta_disc):
+    def __init__(self, num_rays, fov, scan_std, theta_disc, ros_map, res, max_range_m):
         """
-            num_beams  - Number of beams for the simulated LiDAR.
-            fov        - Field of view
-            scan_std   - The standard deviation of the scan
-            rt_e       - Ray tracing Epsilon
-            theta_disc - Theta Discretization
+            num_rays    - Number of beams for the simulated LiDAR.
+            fov         - Field of view
+            scan_std    - The standard deviation of the scan
+            theta_disc  - Theta Discretization
+            ros_map     - Map of the track
+            res         - Resolution of the map
+            max_range_m - Max distance in meters
         """
 
         # these were used in the old scan 2d
-        self.num_rays   = num_rays
-        self.fov        = fov
-        self.scan_std   = scan_std
-        self.rt_e       = rt_e
+        self.num_rays = num_rays
+        self.fov = fov
+        self.scan_std = scan_std
         self.theta_disc = theta_disc
-        self.twopi      = math.pi * 2
+        self.ros_map = ros_map
+        self.max_range_m = max_range_m
+        self.res = resolution
 
-        # the vars above should be ros params..
+        # often used
+        self.twopi = math.pi * 2
 
-        # get map stuff here
-        # wait until map loaded...
-        # or pass map as a param...
+        # create map object
+        self.omap = range_libc.PyOMap(ros_map)
+        self.mrx = int(self.max_range_m / self.res)
 
+        # create ray marching object
+        self.scan_method = range_libc.PyRayMarchingGPU(oMap, self.mrx)
 
+        self.output_vector = np.ones(self.num_rays)
 
-        self.scan_method = range_libc.PyRayMarchingGPU(oMap, self.max_rpx, self.theta_disc)
+    def getMap(self):
+        """
+            Read current map from ROS,
+            add dynamic map here aswell??
+        """
 
-    def initMap(self, map_msg):
-        # init map first time
-        pass
+        # this should not be here
 
-    def setMap(self, map_msg):
-        # there should be more stuff here
+        map_service_name = rospy.get_param("~static_map", "static_map")
+        rospy.wait_for_server(map_service_name)
+        map_msg = rospy.ServiceProxy(map_service_name, GetMap)().map
+
+        self.map_info = map_msg.info
         self.omap = range_libc.PyOMap(map_msg)
+        self.max_range_px = int(self.max_range_m / self.map_info.resolution)
 
-    def scan(self, x, y, pose):
+    def scan(self, x, y, theta):
         """
-            TODO:
-                    Load map
-                    Test functionality
+            x - Position x on the map
+            y - Position y on the map
+            theta - Direction on the map
         """
 
-        theta_index = self.theta_disc*(pose.theta - self.fov/2.0)/self.twopi
+        theta_index = self.theta_disc*(theta - self.fov/2.0)/self.twopi
         thetas = []
         for _ in xrange(self.num_rays):
 
             thetas.append(theta_index)
-
             theta_index += self.theta_disc
+
             # wrap around
             while theta_index >= self.theta_disc:
                 theta_index -= self.theta_disc
-        input_vector = map(thetas, lambda t:(x, y, t))
-        self.output_vector = np.ones(self.num_rays)
+
+        # create numpy array
+        input_vector = np.array(map(thetas, lambda t:(x, y, t)))
 
         # run ray marching
-        self.scan_method.numpy_calc_range(input_vector, self.output_vector)
+        self.scan_method.numpy_calc_range(input_vector,
+                                          self.output_vector,
+                                          num_rays)
 
-
+        # add some noise to the output
+        self.output_vector = np.random.normal(self.output_vector,
+                                              scale=self.scan_std)
         return self.output_vector
 
 
