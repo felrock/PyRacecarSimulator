c554e0e519972b64c7e1efe7bd450c18b2bdbec1
jonajo15 <jonajo15@student.hh.se>
2020-04-22 13:44:15 +0200

    racecarSIM pot complete

--- scripts/racecar_simulator.py
+++ scripts/racecar_simulator.py
@@ -1,5 +1,6 @@
 import * from car_config
 import numpy as np
+import math
 from scan_simulator import ScanSimulator2D
 import precompute
 import kinematics
@@ -86,7 +87,7 @@ class RacecarSimulator():
 
         def update_pose(this):
             this.compute_accel(this.desired_speed)
-            this.set_steer_angle_vel(this.compute_steer_vel(this.desired_steer_ang))
+            this.compute_steer_vel(this.desired_steer_ang)
 
             #update pose
             this.state = kinematics.ST_update(
@@ -95,24 +96,45 @@ class RacecarSimulator():
                             this.steer_ang_vel,
                             this.params,
                             0.001)# current-prev
+            this.state.velocity = set_bounded(this.state.velocity, this.max_speed)
+            this.state.steer_angle = set_bounded(this.state.steer_angle, this.max_steering_angle)
+
+            x = this.state.x + this.scan_distance_to_base_link * math.cos(this.state.theta)
+            y = this.state.y + this.scan_distance_to_base_link * math.sin(this.state.theta)
+
+            scan = this.scan_simulator.scan(x, y, this.state.theta)
+
+            no_collision = True
+
+            if(this.state.velocity != 0):
+                for( x in range(len(scan))):
+                    proj_velocity = this.state.velocity * this.cosines[x]
+                    ttc = (scan[x] - this.car_distances[x]) / proj_velocity
+                if((ttc < this.ttc_threshold) and (ttc >= 0.0)):
+                    if not(ttc):
+                        this.first_ttc_actions()
+                    no_collision = False
+                    this.TTC = True
+
+                    print("Collision detected")
+                    
+            if(no_collision):
+                this.TTC = False
 
 
-        #SET FUNCTIONS
-        def set_accel(self, accel):
-            this.accel = min(max(accel, - this.max_accel), max_accel)
-
-        def set_steer_angle_vel(self, steer_angle_vel):
-            this.steer_angle_vel = min(max(steer_angle_vel, -this.max_steering_vel), this.max_steering_vel)
-
+        #SET FUNCTION
+        @static
+        def set_bounded(value, max_value):
+            return min(max(value, -max_value), max_value)
 
         #HELPER FUNCTIONS
         def compute_steer_vel(self, desired_angle):
             dif = (desired_angle - this.state.steer_angle)
 
             if(abs(dif) > 0.0001):
-                return dif / abs(dif) * this.max_steering_vel
+                this.steer_angle_vel = set_bounded((dif / abs(dif) * this.max_steering_vel), this.max_steering_vel)
             else:
-                return 0
+                this.steer_angle_vel = set_bounded(0, this.max_steering_vel)
 
         def compute_accel(self, desired_velocity):
             dif = desired_velocity - this.state.velocity
@@ -120,14 +142,23 @@ class RacecarSimulator():
             if(this.state.velocity > 0):
                 if(dif > 0):
                     kp = 2.0 * this.max_accel / this.max_speed
-                    this.set_accel(kp_dif)
+                    this.accel = set_bounded(kp*dif, this.max_accel)
                 else:
-                    #brake
-                    accel = -this.max_decel
+                    accel = -this.max_decel #brake
             else:
                 if (dif > 0):
-                    #brake
-                    accel = -this.max_decel
+                    accel = -this.max_decel #brake
                 else:
                     kp = 2.0 * this.max_accel / this.max_speed
-                    this.set_accel(kp_dif)
+                    this.accel = set_bounded(kp*dif, this.max_accel)
+
+        def first_ttc_actions(self):
+            #completely stop vehicle
+            this.state.velocity = 0.0
+            this.state.angular_velocity = 0.0
+            this.state.slip_angle = 0.0
+            this.state.steer_angle = 0.0
+            this.steer_angle_vel = 0.0
+            this.accel = 0.0
+            this.desired_speed = 0.0
+            this.desired_steer_ang = 0.0
